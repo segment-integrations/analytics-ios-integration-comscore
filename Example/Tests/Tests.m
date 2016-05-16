f00a8ef49934df4ee4990a3d5fcd4ba9375f6eb5
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-05-16 13:07:12 -0700

    basic initialization tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -10,36 +10,29 @@
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
+describe(@"SEGComScoreIntegrationFactory", ^{
+    it(@"factory creates integration with empty settings", ^{
+        SEGComScoreIntegration *integration = [[SEGComScoreIntegrationFactory instance] createWithSettings:@{
+                                                                                                               } forAnalytics:nil];
         
-        });
+        expect(integration.settings).to.equal(@{});
     });
 });
 
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^{
-        waitUntil(^(DoneCallback done) {
-            done();
+describe(@"SEGComScoreIntegrationFactory", ^{
+    it(@"factory creates integration with basic settings", ^{
+        SEGComScoreIntegration *integration = [[SEGComScoreIntegrationFactory instance] createWithSettings:@{
+            @"customerC2" : @"1234567",
+            @"publisherSecret": @"publisherSecretString",
+            @"setSecure": @"1",
+            @"autoUpdateInterval": @"background"
+        } forAnalytics:nil];
+        
+        expect(integration.settings).to.equal(@{
+            @"customerC2": @"1234567",
+            @"publisherSecret": @"publisherSecretString",
+            @"setSecure": @"1",
+            @"autoUpdateInterval": @"background"
         });
     });
 });
