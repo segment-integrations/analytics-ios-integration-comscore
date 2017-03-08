f9ef72cbb5995e78ecc71c9bc87d86796c194db2
Tony Xiao <tonyx.ca@gmail.com>
2017-03-07 16:44:14 -0800

    Migrate to new Comscore SDK calls

--- Segment-ComScore/Classes/SEGComScoreIntegration.m
+++ Segment-ComScore/Classes/SEGComScoreIntegration.m
@@ -15,122 +15,90 @@
 {
     if (self = [super init]) {
         self.settings = settings;
-//        self.comScoreClass = [CSComScore class];
-//        
-//        [self.comScoreClass setAppContext];
-//        SEGLog(@"[CSComScore setAppContext]");
-//        [self.comScoreClass setCustomerC2:[self customerC2]];
-//        SEGLog(@"[CSComScore setCustomerC2: %@]", [self customerC2]);
-//        [self.comScoreClass setPublisherSecret:[self publisherSecret]];
-//        SEGLog(@"[CSComScore setPublisherSecret: %@]", [self publisherSecret]);
-//        [self.comScoreClass setAppName: [self appName]];
-//        SEGLog(@"[CSComScore setAppName: %@]", [self appName]);
-//        [self.comScoreClass setSecure: [self useHTTPS]];
-//        SEGLog(@"[CSComScore setSecure: %@]", [self useHTTPS] ? @YES : @NO);
-//        if ([self autoUpdate]) {
-//            if ([self foregroundOnly]) {
-//                [self.comScoreClass enableAutoUpdate:[self autoUpdateInterval] foregroundOnly: YES];
-//                SEGLog(@"[CSComScore enableAutoUpdate: %@ foregroundOnly: YES]", [self autoUpdateInterval]);
-//            } else {
-//                [self.comScoreClass enableAutoUpdate:[self autoUpdateInterval] foregroundOnly: NO];
-//                SEGLog(@"[CSComScore enableAutoUpdate: %@ foregroundOnly: NO]", [self autoUpdateInterval]);
-//            }
-//        }
+        
+        SCORPublisherConfiguration *config = [SCORPublisherConfiguration publisherConfigurationWithBuilderBlock:^(SCORPublisherConfigurationBuilder * builder) {
+            builder.publisherId = settings[@"appName"]; // @ladan please config this is actually the case. Or we need new config with publisherId
+            builder.publisherSecret = settings[@"publisherSecret"];
+            
+            
+            if ([settings[@"autoUpdate"] boolValue] && [settings[@"foregroundOnly"] boolValue]) {
+                builder.usagePropertiesAutoUpdateMode = SCORUsagePropertiesAutoUpdateModeForegroundOnly;
+            } else if ([settings[@"autoUpdate"] boolValue]) {
+                builder.usagePropertiesAutoUpdateMode = SCORUsagePropertiesAutoUpdateModeForegroundAndBackground;
+            } else {
+                builder.usagePropertiesAutoUpdateMode = SCORUsagePropertiesAutoUpdateModeDisabled;
+            }
+            builder.usagePropertiesAutoUpdateInterval = [settings[@"autoUpdateInterval"] integerValue];
+            
+            builder.secureTransmission = [settings[@"useHTTPS"] boolValue];
+            
+            //        TODO: What are these? And do they have equivalents
+            //        [self.comScoreClass setAppContext];
+            //        SEGLog(@"[CSComScore setAppContext]");
+            //        [self.comScoreClass setCustomerC2:[self customerC2]];
+            //        SEGLog(@"[CSComScore setCustomerC2: %@]", [self customerC2]);
+            //        [self.comScoreClass setPublisherSecret:[self publisherSecret]];
+            //        SEGLog(@"[CSComScore setPublisherSecret: %@]", [self publisherSecret]);
+            //        [self.comScoreClass setAppName: [self appName]];
+            //        SEGLog(@"[CSComScore setAppName: %@]", [self appName]);
+
+        }];
+        [[SCORAnalytics configuration] addClientWithConfiguration:config];
+        
+        [SCORAnalytics start];
+        
     }
     return self;
 }
 
-//- (instancetype)initWithSettings:(NSDictionary *)settings andCSComScore:(Class)comScoreClass
-//{
-//    if (self = [super init]) {
-//        self.settings = settings;
-//        self.comScoreClass = comScoreClass;
-//    }
-//    return self;
-//
-//}
-//
-//+ (NSDictionary *)mapToStrings:(NSDictionary *)dictionary
-//{
-//    NSMutableDictionary *mapped = [NSMutableDictionary dictionaryWithDictionary:dictionary];
-//    
-//    [dictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
-//        id data = [mapped objectForKey:key];
-//        if (!![data isKindOfClass:[NSString class]]) {
-//            [mapped setObject:[NSString stringWithFormat:@"%@", data] forKey:key];
-//        }
-//    }];
-//    
-//    return [mapped copy];
-//}
-//
-//- (void)identify:(SEGIdentifyPayload *)payload
-//{
-//    NSDictionary *mappedTraits = [SEGComScoreIntegration mapToStrings:payload.traits];
-//    [mappedTraits enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
-//        id data = [payload.traits objectForKey:key];
-//        if (data != nil && [data length] != 0) {
-//            [self.comScoreClass setLabel: key value: data];
-//            SEGLog(@"[CSComScore setLabel: %@ value: %@]", key, data);
-//        }
-//    }];
-//}
-//
-//
-//- (void)track:(SEGTrackPayload *)payload
-//{
-//    NSMutableDictionary *hiddenLabels = [@{@"name":payload.event} mutableCopy];
-//    [hiddenLabels addEntriesFromDictionary:[SEGComScoreIntegration mapToStrings:payload.properties]];
-//    [self.comScoreClass hiddenWithLabels:hiddenLabels];
-//    
-//}
-//
-//- (void)screen:(SEGScreenPayload *)payload
-//{
-//    NSMutableDictionary *viewLabels = [@{@"name":payload.name} mutableCopy];
-//    [viewLabels addEntriesFromDictionary:[SEGComScoreIntegration mapToStrings:payload.properties]];
-//    [self.comScoreClass viewWithLabels:viewLabels];
-//}
-//
-//- (void)flush
-//{
-//    SEGLog(@"ComScore flushCache");
-//    [self.comScoreClass flushCache];
-//}
-//
-//- (NSString *)customerC2
-//{
-//    return self.settings[@"c2"];
-//}
-//
-//- (NSString *)publisherSecret
-//{
-//    return (NSString *)[self.settings objectForKey:@"publisherSecret"];
-//}
-//
-//- (NSString *)appName
-//{
-//    return (NSString *)[self.settings objectForKey:@"appName"];
-//}
-//
-//- (BOOL)autoUpdate
-//{
-//    return [(NSNumber *)[self.settings objectForKey:@"autoUpdate"] boolValue];
-//}
-//
-//- (BOOL)foregroundOnly
-//{
-//    return [(NSNumber *)[self.settings objectForKey:@"foregroundOnly"] boolValue];
-//}
-//
-//- (int)autoUpdateInterval
-//{
-//    return [(NSNumber *)[self.settings objectForKey:@"autoUpdateInterval"] intValue];
-//}
-//
-//- (BOOL)useHTTPS
-//{
-//    return [(NSNumber *)[self.settings objectForKey:@"useHTTPS"] boolValue];
-//}
+
++ (NSDictionary *)mapToStrings:(NSDictionary *)dictionary
+{
+    NSMutableDictionary *mapped = [NSMutableDictionary dictionaryWithDictionary:dictionary];
+
+    [dictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
+        id data = [mapped objectForKey:key];
+        if (!![data isKindOfClass:[NSString class]]) {
+            [mapped setObject:[NSString stringWithFormat:@"%@", data] forKey:key];
+        }
+    }];
+    
+    return [mapped copy];
+}
+
+- (void)identify:(SEGIdentifyPayload *)payload
+{
+    NSDictionary *mappedTraits = [SEGComScoreIntegration mapToStrings:payload.traits];
+    [mappedTraits enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
+        id data = [payload.traits objectForKey:key];
+        if (data != nil && [data length] != 0) {
+            [[SCORAnalytics configuration] setPersistentLabelWithName:key value:data];
+            SEGLog(@"[[SCORAnalytics configuration] setPersistentLabelWithName: %@ value: %@]", key, data);
+        }
+    }];
+}
+
+
+- (void)track:(SEGTrackPayload *)payload
+{
+    NSMutableDictionary *hiddenLabels = [@{@"name": payload.event} mutableCopy];
+    [hiddenLabels addEntriesFromDictionary:[SEGComScoreIntegration mapToStrings:payload.properties]];
+    [SCORAnalytics notifyHiddenEventWithLabels:hiddenLabels];
+    
+}
+
+- (void)screen:(SEGScreenPayload *)payload
+{
+    NSMutableDictionary *viewLabels = [@{@"name":payload.name} mutableCopy];
+    [viewLabels addEntriesFromDictionary:[SEGComScoreIntegration mapToStrings:payload.properties]];
+    [SCORAnalytics notifyViewEventWithLabels:viewLabels];
+}
+
+
+- (void)flush
+{
+    SEGLog(@"ComScore flushCache");
+    [SCORAnalytics flushOfflineCache];
+}
 
 @end
