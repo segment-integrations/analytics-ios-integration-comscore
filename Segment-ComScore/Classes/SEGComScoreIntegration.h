a77f299b0f1bf804e9c0dd249b9470bee2ec46bd
Ladan Nasserian <ladanazita@users.noreply.github.com>
2017-06-02 12:24:26 -0700

    Add video tracking (#14)

--- Segment-ComScore/Classes/SEGComScoreIntegration.h
+++ Segment-ComScore/Classes/SEGComScoreIntegration.h
@@ -11,11 +11,25 @@
 #import <ComScore/ComScore.h>
 
 
+@protocol SEGStreamingAnalyticsFactory <NSObject>
+- (SCORStreamingAnalytics *)create;
+@end
+
+
+@interface SEGRealStreamingAnalyticsFactory : NSObject <SEGStreamingAnalyticsFactory>
+@end
+
+
 @interface SEGComScoreIntegration : NSObject <SEGIntegration>
 
 @property (nonatomic, strong) NSDictionary *settings;
 @property (nonatomic, strong) Class scorAnalyticsClass;
+@property (nonatomic, strong) SCORStreamingAnalytics *streamAnalytics;
+@property (nonatomic) id<SEGStreamingAnalyticsFactory> streamingAnalyticsFactory;
+NSNumber *convertFromKBPSToBPS(NSDictionary *src, NSString *key);
 
-- (instancetype)initWithSettings:(NSDictionary *)settings andComScore:(id)scorAnalyticsClass;
+
+- (instancetype)initWithSettings:(NSDictionary *)settings andComScore:(id)scorAnalyticsClass andStreamingAnalyticsFactory:(id<SEGStreamingAnalyticsFactory>)streamingAnalyticsFactory;
+
 
 @end
